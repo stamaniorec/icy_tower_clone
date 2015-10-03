582adb37f0ded991f8a83d5fdc6f7062cffe1c73
dimiturtrz <dimitar.trz@gmail.com>
2015-10-03 13:49:12 +0300

    sensible placement

--- PlatformController.py
+++ PlatformController.py
@@ -1,22 +1,24 @@
 import pygame
 from random import randrange
 from Platform import Platform
-from Constants import SCREEN_WIDTH, JUMP_VELOCITY
+from Constants import SCREEN_WIDTH, JUMP_VELOCITY, MAX_JUMP
 pygame.init()
 
 class PlatformController:
-	platform_set = []
-	
+	platform_set = []	
+
 	def __init__(self):
+		self.last_x = MAX_JUMP
 		for i in range(0, 10):
 			self.platform_set.append(self.generate_platform(i))
 		self.flags = []
 	
 	def generate_platform(self, index):
-		width=500
+		width=70
 		height=20
 		y = index * 100
-		x = randrange(0, SCREEN_WIDTH-width) 
+		x = randrange(self.last_x-MAX_JUMP , self.last_x+MAX_JUMP+width) #SCREEN_WIDTH-width)
+		self.last_x = x
 		return Platform(x, y, width, height)
 
 	def draw(self, game_display):
